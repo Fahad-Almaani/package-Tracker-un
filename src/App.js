46eb072666dd5773fa943f111ce469cf1994942b
Fahad-Almaani <fahad.almaani.fa@gmail.com>
2023-11-29 18:41:54 +0400

    before the merge

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import "./App.css";
 import Navbar from "./Components/NavBar/Navbar";
-import Home from "./Pages/Home/Home";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Landing from "./Pages/Landing/Landing";
+import About from "./Pages/About/About";
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route element={<Home/>} path="/home"/>
-          <Route element={<Home/>} path="/login"/>
+          <Route element={<Landing/>} path="/"/>
           <Route element={<Home/>} path="/signup"/>
+          <Route path="/about" element={<About/>}></Route>
         </Routes>
       </BrowserRouter>
       {/* <Navbar /> */}
